ec43569fd21ab14b5108d3f5d645245ff531e2a5
김재성 <aamoos@openmate.co.kr>
2024-07-05 13:13:04 +0900

    4강. 사칙연산 계산기에 대해 테스트 코드 작성하기

---
+++ src/test/kotlin/com/group/libaryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,25 @@
+package com.group.libaryapp.calculator
+
+import com.group.libraryapp.calculator.Calculator
+
+fun main(){
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+}
+
+class CalculatorTest {
+
+    fun addTest(){
+        //given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        //then
+        if(calculator.number != 8){
+            throw IllegalStateException()
+        }
+    }
+
+}
