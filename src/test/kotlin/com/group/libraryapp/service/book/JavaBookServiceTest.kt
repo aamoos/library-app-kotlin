7cf48d0561074fc3d0c9ba2d143c030c75476640
김재성 <aamoos@openmate.co.kr>
2024-07-15 14:26:57 +0900

    13강. 도메인 계층을 Kotlin으로 변경하기 - UserLoanHistory.java, User.java

--- src/test/kotlin/com/group/libraryapp/service/book/JavaBookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/JavaBookServiceTest.kt
@@ -9,7 +9,6 @@ import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
@@ -19,7 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BookServiceTest @Autowired constructor(
+class JavaBookServiceTest @Autowired constructor(
     private val bookService: BookService,
     private val bookRepository: BookRepository,
     private val userRepository: UserRepository,
