e986f443bb9d30d0c179824d7d4913fa69f693d1
kimjaesung <aamoos@naver.com>
2024-07-25 13:47:48 +0900

    강의

--- src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -2,7 +2,7 @@ package com.group.libraryapp.domain.book
 
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.Optional
-
+//test
 interface BookRepository : JpaRepository<Book, Long> {
 
     fun findByName(bookName: String): Book?
